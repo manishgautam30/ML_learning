c980f509fb8375810f72948fcb9b6f36fca81e32
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-07-21 23:03:46 +0530

    taking last 40 values to train model and first 40 to test

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import matplotlib as plt
 import numpy as np
 from sklearn import datasets, linear_model
 
-diabetes=datasets.load_diabetes()
-print(diabetes.DESCR)
-diabetes_X=diabetes.data[:,np.newaxis,2]
+diabetes = datasets.load_diabetes()
+diabetes_X = diabetes.data[:, np.newaxis, 2]
+
+diabetes_X_train = diabetes_X[:-40]
+diabetes_X_test = diabetes_X[-40:]
+
+diabetes_Y_train = diabetes.target[:-40]
+diabetes_Y_test = diabetes.target[-40:]
+
