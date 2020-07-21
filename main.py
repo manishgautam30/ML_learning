be6e892e4196ffb443d648948b867aaf09ba427e
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-07-21 23:43:13 +0530

    commented out the plot cause using multivariable linear regression

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error
 
 diabetes = datasets.load_diabetes()
-diabetes_X = diabetes.data[:, np.newaxis, 2]
+diabetes_X = diabetes.data
 
 diabetes_X_train = diabetes_X[:-40]
 diabetes_X_test = diabetes_X[-40:]
@@ -23,8 +23,8 @@ print("mean squared error is : ",mean_squared_error(diabetes_Y_test, diabetes_Y_
 print('weights',model.coef_)
 print('intercept', model.intercept_)
 
-plt.scatter(diabetes_X_test,diabetes_Y_test)
-plt.plot(diabetes_X_test, diabetes_Y_predict)
-plt.xlabel('diabetes_X_test')
-plt.ylabel('diabetes_Y_predict')
-plt.show()
+# plt.scatter(diabetes_X_test,diabetes_Y_test)
+# plt.plot(diabetes_X_test, diabetes_Y_predict)
+# plt.xlabel('diabetes_X_test')
+# plt.ylabel('diabetes_Y_predict')
+# plt.show()
