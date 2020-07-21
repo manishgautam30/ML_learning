c6db357a9aa738e9474c1fd472e269154d27f6fc
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-07-21 22:58:32 +0530

    getting numpy arrays in diabetes_X

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import matplotlib as plt
 import numpy as np
 from sklearn import datasets, linear_model
+
+diabetes=datasets.load_diabetes()
+print(diabetes.DESCR)
+diabetes_X=diabetes.data[:,np.newaxis,2]
